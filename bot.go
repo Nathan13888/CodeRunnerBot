f69fc9071bd3abb9661dac9f1ae30a18ddb77d61
Vidhan Bhatt <me@vidhan.io>
2021-11-29 09:40:48 -0500

    remove duplicate sigterm

--- bot.go
+++ bot.go
@@ -81,7 +81,7 @@ func main() {
 	// Wait here until CTRL-C or other term signal is received.
 	log.Info().Msg("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
 	for _, cmd := range commands {
