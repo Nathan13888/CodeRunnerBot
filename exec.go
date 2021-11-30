63821794af89514cca672902f88617613f6e2f25
Vidhan Bhatt <me@vidhan.io>
2021-11-30 09:55:05 -0500

    use piston endpoint for languages rather than hardcoding them. (#6)
    * use piston endpoint for languages rather than hardcoding them.
    
    * no hardcoded anything anymore!

--- exec.go
+++ exec.go
@@ -8,20 +8,26 @@ import (
 
 func Exec(lang string, version string, code string) (string, error) {
 	httpClient := http.DefaultClient
-	client := piston.New("",
-		httpClient,
-		PISTON_URL,
-	)
+	client := piston.New("", httpClient, PISTON_URL)
 	output, err := client.Execute(lang, version,
 		[]piston.Code{
 			{
 				Content: code,
 			},
 		},
-		// piston.Stdin("hello world"), // Passing input as "hello world".
 	)
 	if err != nil {
 		return "", err
 	}
 	return output.GetOutput(), nil
+}
+
+func GetRuntimes() *piston.Runtimes {
+	httpClient := http.DefaultClient
+	client := piston.New("", httpClient, PISTON_URL)
+	runtimes, err := client.GetRuntimes()
+	if err != nil {
+		return nil
+	}
+	return runtimes
 }
